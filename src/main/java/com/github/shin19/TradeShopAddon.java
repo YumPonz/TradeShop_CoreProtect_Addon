74729fe02009a4d3534956461f6633ce80c1ad1b
shin19 <github@shin19.net>
2021-07-04 21:17:33 +0900

    Release v1.0.0

---
+++ src/main/java/com/github/shin19/TradeShopAddon.java
@@ -0,0 +1,48 @@
+package com.github.shin19;
+
+import net.coreprotect.CoreProtect;
+import net.coreprotect.CoreProtectAPI;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.shanerx.tradeshop.framework.events.PlayerTradeEvent;
+
+public class TradeShopAddon extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+    }
+    @EventHandler
+    public void onPlayerTrade(PlayerTradeEvent event) {
+        CoreProtectAPI coreProtect = getCoreProtect();
+        if (coreProtect != null) {
+            coreProtect.logContainerTransaction(event.getPlayer().getDisplayName(), event.getShop().getInventoryLocationAsSL().getLocation());
+        }
+    }
+    private CoreProtectAPI getCoreProtect() {
+        Plugin plugin = getServer().getPluginManager().getPlugin("CoreProtect");
+
+        // Check that CoreProtect is loaded
+        if (!(plugin instanceof CoreProtect)) {
+            return null;
+        }
+
+        // Check that the API is enabled
+        CoreProtectAPI CoreProtect = ((CoreProtect) plugin).getAPI();
+        if (!CoreProtect.isEnabled()) {
+            return null;
+        }
+
+        // Check that a compatible version of the API is loaded
+        if (CoreProtect.APIVersion() < 6) {
+            return null;
+        }
+
+        return CoreProtect;
+    }
+}
